dcea32bc29681bac4fd72d9999d186da3cf2fc24
Davis JR <davisjr2000@hotmail.com>
2018-10-29 18:09:12 -0300

    Added more style/pages

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,18 +2,27 @@ require 'open-uri'
 require 'json'
 
 class GamesController < ApplicationController
+  def index
+    session[:score] = 0
+    session[:attempts] = 0
+  end
   def new
-    @letters = (0...9).map { rand(65..90).chr }
+    letters = (0...7).map { rand(65..90).chr }
+    2.times do
+      letters << ["A", "E", "I", "O", "U"].sample
+    end
+    @letters = letters.shuffle
     @start_time = Time.now
   end
 
   def score
+    session[:attempts] += 1
     @attempt = params['attempt']
     @letters = params[:letters]
     @start_time = params[:start_time]
     if @attempt.nil? || @attempt == ""
       @result = "Type something you stupid piece of shit"
-      @score ="0, you didn't even try lol"
+      @score = -1
     else
       @attempt = @attempt.capitalize
       url = 'https://wagon-dictionary.herokuapp.com/' + @attempt
@@ -30,15 +39,15 @@ class GamesController < ApplicationController
       end
       if data['found'] == false
         @result = "#{@attempt} is not an English Word"
-        @score = 0
+        @score = -3
         @message = "Learn to write"
       elsif data['found'] == true && gridok == false
         @result = "#{@attempt} is not in the grid"
-        @score = 0
+        @score = - 5
         @message = "Learn to read"
       else
         @result = "Well Done! #{@attempt} was a great choice"
-        @score = 5 * @attempt.length - (Time.now - @start_time.to_datetime)
+        @score = (5 * @attempt.length - (Time.now - @start_time.to_datetime)).round(2)
         if @score < 0
           if @score > -1
             @message = "Slow huh"
@@ -52,5 +61,7 @@ class GamesController < ApplicationController
         end
       end
     end
+    session[:score] += @score
+    redirect_to final_path if session[:score] >= 50
   end
 end
